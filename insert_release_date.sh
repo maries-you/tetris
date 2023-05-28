0cf17705aeac0c992f9ddf87f66f02963b6b61e9
thehighestmath <kirill.lfybk.rh@gmail.com>
2023-05-28 12:40:28 +0000

    fix script

--- insert_release_date.sh
+++ insert_release_date.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-date=git log --pretty=format:"%ad" --date=format:"%d.%m.%Y %H:%M" -n 1
+date=$(git log --pretty=format:"%ad" --date=format:"%d.%m.%Y %H:%M" -n 1)
 search="{{ release_date }}"
-filename="index.html"
+filename="game.html"
 
 sed -i "s/$search/$date/" $filename

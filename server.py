2aae6b70943455c3626e117f8e309969693fa49a
Marina Yuzhaninova <133424277+maries-you@users.noreply.github.com>
2023-06-04 21:05:09 +0300

    upgrade ci for remote server

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
+DATABASE_PATH = 'database.json'
+
 
 @cross_origin
 @app.route('/')
@@ -14,7 +16,7 @@ def hello_world():
 
 @app.route('/records', methods=['GET'])
 def get_records():
-    with open('database.json', 'r', encoding='utf8') as read_file:
+    with open(DATABASE_PATH, 'r', encoding='utf8') as read_file:
         records = json.load(read_file)
     records_data = json.dumps(records, ensure_ascii=False)
     return Response(records_data, content_type='application/json')
@@ -22,11 +24,11 @@ def get_records():
 
 @app.route('/records', methods=['POST'])
 def add_record():
-    with open('database.json', 'r', encoding='utf8') as read_file:
+    with open(DATABASE_PATH, 'r', encoding='utf8') as read_file:
         records = json.load(read_file)
     print(request.json)
     records.append(request.json)
-    with open('database.json', 'w', encoding='utf8') as write_file:
+    with open(DATABASE_PATH, 'w', encoding='utf8') as write_file:
         json.dump(records, write_file, indent=4, ensure_ascii=False)
     return 'success'
 
